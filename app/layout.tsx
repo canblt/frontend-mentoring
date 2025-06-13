1528baf762c679d3e07abf5714a4957f09b48bf2
Can Bulut <can@MacBookPro.fritz.box>
2025-06-13 15:19:31 +0200

    fix: nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactNode, useState } from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '@/config/theme';
-import { Navigation } from '@/components/Navigation';
-import { lato } from '@/config/fonts';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/config/theme";
+import { lato } from "@/config/fonts";
+import Navigation from "@/components/Navigation/Navigation";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <html
-      lang="en"
-      className={lato.className}
-    >
+    <html lang="en" className={lato.className}>
       <body>
         <QueryClientProvider client={queryClient}>
           <AppRouterCacheProvider options={{ enableCssLayer: true }}>
